7261d41eff93af16754c486a8ccf0332a090bb88
fepegar <fepegar@gmail.com>
2017-03-29 16:15:52 +0200

    Set blockmatching path to ~/bin/blockmatching

--- Blockmatching.py
+++ Blockmatching.py
@@ -15,7 +15,8 @@ from slicer.ScriptedLoadableModule import *
 import sys
 
 
-BLOCKMATCHING_PATH = '/home/fernando/git/morpheme-privat/vt/build/bin/blockmatching'
+# BLOCKMATCHING_PATH = '/home/fernando/git/morpheme-privat/vt/build/bin/blockmatching'
+BLOCKMATCHING_PATH = os.path.expanduser('~/bin/blockmatching')
 RESULT_NAME = 'Blockmatching result'
 
 TRANSFORMATIONS_MAP = collections.OrderedDict([
@@ -535,3 +536,19 @@ class BlockmatchingLogic(ScriptedLoadableModuleLogic):
         line = '\n'.join(lines)
         with open(trsfPath, 'w') as f:
             f.write(line)
+
+
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
